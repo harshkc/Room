f0f6bc9b4f372498a3b15791306fb1824f05ff67
harshkc <harsh.hkc1006@gmail.com>
2021-09-13 23:43:50 +0530

    added Video Grid UI

--- src/Video.js
+++ src/Video.js
@@ -1,5 +1,38 @@
-const Video = () => {
-  return <div></div>;
+import { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
+import { AgoraVideoPlayer } from "agora-rtc-react";
+
+const Video = ({ tracks, user }) => {
+  const [gridSpacing, setGridSpacing] = useState(12);
+
+  useEffect(() => {
+    setGridSpacing(Math.max(Math.floor(12 / user.length + 1), 4));
+  }, [tracks, user]);
+
+  return (
+    <Grid container style={{ height: "100%" }}>
+      <Grid item xs={gridSpacing}>
+        <AgoraVideoPlayer
+          style={{ height: "100", width: "100" }}
+          videoTrack={tracks[1]}
+        />
+      </Grid>
+      {user.length > 0 &&
+        user.map((user) => {
+          if (user.track) {
+            return (
+              <Grid item xs={gridSpacing}>
+                <AgoraVideoPlayer
+                  key={user.uid}
+                  style={{ height: "100", width: "100" }}
+                  videoTrack={user.track}
+                />
+              </Grid>
+            );
+          } else return null;
+        })}
+    </Grid>
+  );
 };
 
 export default Video;
